84fd28dc66fad4286ea2bb288e60f8c15a246946
Dave Pierre <35567910+DavePierre101014409@users.noreply.github.com>
2018-06-12 12:58:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
+# registers 
+register =[]
 @app.route("/")
 def homePage():
     return render_template("Homepage.html")
@@ -11,7 +11,20 @@ def homePage():
 def signPage():
     return render_template("SignInPage.html")
 
+@app.route("/register", methods=["POST"])
+def register():
+    firstname = request.form.get("")
+    lastName = request.form.get("")
+    emailName = request.form.get("")
+    if not firstname or not lastName or not emailName :
+        # return failure
+        return render_template("")
+    students.append()
+    
+    return redirect("/main")
 
-
+@app.route("/main")
+def main():
+    return render_template("main.html",register=register)
 if __name__== '__main__':
     app.run(debug=True)
