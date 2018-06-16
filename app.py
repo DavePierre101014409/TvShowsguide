f513848990a66430a37fd0118706aaf34a66c147
Dave Pierre <davepierre@Daves-MacBook-Pro.local>
2018-06-16 10:08:28 -0400

    sql databses working

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
-import os 
+import os
 import smtplib
+import sqlite3
 
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
+
+
 
 app = Flask(__name__)
-# registers 
+# registers
 register =[]
+
+@app.route("/a")
+def getData():
+    conn = sqlite3.connect('../users.db')
+    curs=conn.cursor()
+    curs.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, fName TEXT, lName TEXT)")
+
+    curs.execute("INSERT INTO user (fName ,lName ) VALUES ('Dave','Dace') ")
+
+    for row in curs.execute("SELECT * FROM user"):
+    	time = str(row[0])
+    	temp = row[1]
+        t = row[2]
+        
+
+
+    	return time + temp+ t
+
+    print str(curs.execute("SELECT fName from user"))
+
+    return str(curs.execute("SELECT * from user"))
+    conn.close()
+
+
+
+
+
+
+
 @app.route("/")
 def homePage():
-    return render_template("Homepage.html")
+    return render_template("home.html")
+#    return render_template("Homepage.html")
 
 @app.route("/signIn")
 def signPage():
@@ -23,7 +56,7 @@ def register():
         # return failure
         return render_template("")
     students.append()
-    
+
     return redirect("/main")
 
 @app.route("/main")
